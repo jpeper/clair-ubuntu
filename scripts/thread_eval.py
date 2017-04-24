3369e87aff8fae708608e33ca9b56dc0c263ba04
Jonathan Kummerfeld <jkk@berkeley.edu>
2017-04-24 09:08:00 -0400

    Re-arranging source and starting a new eval script

---
+++ scripts/thread_eval.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+from __future__ import print_function
+
+import argparse
+import logging
+import sys
+
+def read_clusters(filename):
+    ans = {}
+    cfile = "all"
+    for line in open(filename):
+        if ':' in line:
+            cfile = ':'.join(line.split(':')[:-1])
+            line = line.split(":")[-1]
+        cluster = {int(v) for v in line.split()}
+        ans.setdefault(cfile, []).add(cluster)
+    return ans
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Calculate the Variation of Information metric for two clusters.')
+    parser.add_argument('gold', help='File containing the gold clusters, one per line. If a line contains a ":" the start is considered a filename')
+    parser.add_argument('auto', help='File containing the gold clusters, one per line. If a line contains a ":" the start is considered a filename')
+    args = parser.parse_args()
+
+    auto = read_clusters(args.auto)
+    gold = read_clusters(args.gold)
+
+    scores = {}
+    total_points = 0
+    # Output only in one or the other
+    for filename in auto:
+        if filename not in gold:
+            scores[filename] = 0
+            total_points += sum([len(v) for v in auto[filename]])
+    for filename in gold:
+        if filename not in auto:
+            scores[filename] = 0
+            total_points += sum([len(v) for v in gold[filename]])
+    for filename in gold:
+        if filename in auto:
+
+    # Simple, but slow implementation (should use a disjoint-set)
+    clusters = []
+    for line in sys.stdin:
+        parts = line.strip().split()
+        nums = set([int(p) for p in parts if p != '-'])
+
+        to_merge = []
+        for i, cluster in enumerate(clusters):
+            if len(cluster.intersection(nums)) > 0:
+                to_merge.append(i)
+
+        # If we found no intersection, create a new set
+        to_merge.sort(reverse=True)
+        merged = set()
+        for i in to_merge:
+            merged.update(clusters[i])
+            clusters.pop(i)
+        merged.update(nums)
+        clusters.append(merged)
+
+    for cluster in clusters:
+        print(" ".join([str(v) for v in cluster]))
